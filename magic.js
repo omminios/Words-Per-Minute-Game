5cb17ae6ad37f0cfbddf640e95a419ff940a62be
omminios <harrisspeed001@gmail.com>
2024-12-30 18:31:11 -0500

    pulling github source code

--- magic.js
+++ magic.js
@@ -1,5 +1,6 @@
 let score = 0
-let timer = 60
+let timer = 10
+const highscore_api = 'https://rws7lufvk2.execute-api.us-east-1.amazonaws.com/Prod/highscore'
 const Word_API = 'https://random-word-api.herokuapp.com/word'
 const wordElement = document.getElementById('word')
 const wordInputElement = document.getElementById('input')
@@ -7,16 +8,36 @@ const timerElement = document.getElementById('timer')
 const scoreElement = document.getElementById('cscore')
 scoreElement.innerText = score
 
-
 function countdown() {
     timerElement.innerText = `${timer}`
-    if (timer > 0)
+    if (timer > 0){
         timer--
-    else
-        document.getElementById('input').disabled = true    
+    }
+    else {
+        document.getElementById('input').disabled = true
+        if (compare_scores() === true) {
+            let data = {
+                "ID": 0,
+                "Highscore": score
+              }
+            const putdata = async () =>{
+                await fetch(highscore_api, {
+                    method: 'PUT',
+                    body: JSON.stringify(data),
+                    headers: {
+                        "Content-Type":"application/json",
+                        "Access-Control-Allow-Origin": "*",
+                        'Access-Control-Allow-Methods': 'PUT,OPTIONS,GET'
+                    }
+                }) 
+                console.log(data)
+            }
+            putdata()
+        }
+    }
 }
 
-function get_random_word(){
+function get_random_word() {
     return fetch(Word_API)
     .then(response => {
 		if (response.ok) {
@@ -41,15 +62,46 @@ async function split_word() {
     wordInputElement.value = null
 }
 
-function score_tracker(){
+function score_tracker() {
     score = score+ 1
     console.log(score)
     scoreElement.innerText = score
 }
 
-wordInputElement.addEventListener('input', () =>{
-    setInterval(countdown, 1000)
+function retrive_highscore() {
+    fetch(highscore_api)
+    .then(response => {
+    return response.json()
+})
+    .then(data => {
+    const highscore = `${data}`
+    const data_div = document.querySelector('.highscore')
+    data_div.innerHTML = highscore
+    return data
+})
+}
+
+function compare_scores() {
+    const highscore = document.getElementById('highscore').innerText
+    if (score > highscore) {
+        alert("Congragulations you beat the highscore")
+        return true
+    }
+    else {
+        return false
+    }
+        
+}
+
+
+wordInputElement.addEventListener('input', interval_timer = () =>{
+    const ID = setInterval(countdown, 1000)
+    setTimeout( () => {
+        clearInterval(ID)
+    }, 10000 )
 }, {once: true})
+
+
 
 wordInputElement.addEventListener('input', () =>{
     const wordList = wordElement.querySelectorAll('span')
@@ -77,8 +129,10 @@ wordInputElement.addEventListener('input', () =>{
     
     if(correct)
         split_word()
-             
 })
 
 split_word()
 countdown()
+retrive_highscore()
+
+
